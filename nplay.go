810f9e18972dce35e68b693983e0f9b5de43877c
Arturo Filastò <arturo@filasto.net>
2018-07-02 18:06:27 +0200

    Add mooar triggers!!

--- nplay.go
+++ nplay.go
@@ -20,20 +20,58 @@ var timeWarp = flag.Float64("w", 1.0, "Factor to warp time by")
 var oscIP = flag.String("osc-ip", "127.0.0.1", "OSC port")
 var oscPort = flag.Int("osc-port", 3334, "OSC port")
 
-const DIR_UNKNOWN = 0
-const DIR_INCOMING = 1
-const DIR_OUTGOING = 2
+type TriggerType int64
 
-func computeDirection(srcIP net.IP, dstIP net.IP) int32 {
+const (
+	TRIG_UNKNOWN  TriggerType = 1
+	TRIG_INCOMING TriggerType = 10
+	TRIG_OUTGOING TriggerType = 11
+	TRIG_UDP      TriggerType = 20
+	TRIG_TCP_NSA  TriggerType = 30 // Not SYN or ACK
+	TRIG_TCP_SYN  TriggerType = 31
+	TRIG_TCP_ACK  TriggerType = 32
+	TRIG_TCP_FIN  TriggerType = 33
+	TRIG_TCP_RST  TriggerType = 34
+	TRIG_ICMP     TriggerType = 40
+)
+
+func getTriggerName(trig TriggerType) string {
+	switch trig {
+	case TRIG_UNKNOWN:
+		return "/type/unknown"
+	case TRIG_INCOMING:
+		return "/type/incoming"
+	case TRIG_OUTGOING:
+		return "/type/outgoing"
+	case TRIG_UDP:
+		return "/type/udp"
+	case TRIG_TCP_NSA:
+		return "/type/tcp_nsa"
+	case TRIG_TCP_SYN:
+		return "/type/tcp_syn"
+	case TRIG_TCP_ACK:
+		return "/type/tcp_ack"
+	case TRIG_TCP_FIN:
+		return "/type/tcp_fin"
+	case TRIG_TCP_RST:
+		return "/type/tcp_rst"
+	case TRIG_ICMP:
+		return "/type/icmp"
+	default:
+		return "/type/unknown"
+	}
+}
+
+func computeDirection(srcIP net.IP, dstIP net.IP) TriggerType {
 	ip := srcIP
 	_, private24BitBlock, _ := net.ParseCIDR("10.0.0.0/8")
 	_, private20BitBlock, _ := net.ParseCIDR("172.16.0.0/12")
 	_, private16BitBlock, _ := net.ParseCIDR("192.168.0.0/16")
 	isPrivate := private24BitBlock.Contains(ip) || private20BitBlock.Contains(ip) || private16BitBlock.Contains(ip)
 	if isPrivate {
-		return DIR_OUTGOING
+		return TRIG_OUTGOING
 	}
-	return DIR_INCOMING
+	return TRIG_INCOMING
 }
 
 func ip2int(ip net.IP) uint32 {
@@ -43,9 +81,9 @@ func ip2int(ip net.IP) uint32 {
 	return binary.BigEndian.Uint32(ip)
 }
 
-func makeMessage(data []byte, ci gopacket.CaptureInfo) (*osc.Message, error) {
+func makeMessage(data []byte, ci gopacket.CaptureInfo) ([]*osc.Message, error) {
 	var (
-		direction    int32
+		direction    TriggerType
 		transportStr string
 		appLayerStr  string
 		sport        int32
@@ -54,7 +92,21 @@ func makeMessage(data []byte, ci gopacket.CaptureInfo) (*osc.Message, error) {
 		dstIP        net.IP
 		srcIPNum     int32
 		dstIPNum     int32
+		triggers     []TriggerType
+		messages     []*osc.Message
 	)
+	// The trigger signals that we send are the following:
+	// * INCOMING
+	// * OUTGOING
+	// * UDP
+	// * TCP (!SYNC & !ACK)
+	// * TCP (SYN)
+	// * TCP (ACK)
+	// * TCP (FIN)
+	// * TCP (RST) (rare)
+	// * ICMP
+	// triggers = append(triggers, TRIG_UDP)
+
 	length := ci.Length
 
 	packet := gopacket.NewPacket(data, layers.LayerTypeEthernet, gopacket.Default)
@@ -69,7 +121,7 @@ func makeMessage(data []byte, ci gopacket.CaptureInfo) (*osc.Message, error) {
 	// Network layer features
 	network := packet.NetworkLayer()
 	if network == nil {
-		return nil, fmt.Errorf("no network type")
+		return messages, fmt.Errorf("no network type")
 	}
 	networkType := network.LayerType()
 	if networkType == layers.LayerTypeIPv4 {
@@ -81,7 +133,7 @@ func makeMessage(data []byte, ci gopacket.CaptureInfo) (*osc.Message, error) {
 		srcIP = ipv6.SrcIP
 		dstIP = ipv6.DstIP
 	} else {
-		return nil, fmt.Errorf("unsupported network type")
+		return messages, fmt.Errorf("unsupported network type")
 	}
 
 	// Transport layer features
@@ -91,20 +143,49 @@ func makeMessage(data []byte, ci gopacket.CaptureInfo) (*osc.Message, error) {
 		transportType = transport.LayerType()
 	}
 	switch {
+	case transportType == layers.LayerTypeICMPv4:
+		transportStr = "icmp4"
+		triggers = append(triggers, TRIG_ICMP)
+	case transportType == layers.LayerTypeICMPv6:
+		transportStr = "icmp6"
+		triggers = append(triggers, TRIG_ICMP)
 	case transportType == layers.LayerTypeUDP:
 		transportStr = "udp"
 		udp := transport.(*layers.UDP)
 		sport = int32(udp.SrcPort)
 		dport = int32(udp.DstPort)
+		triggers = append(triggers, TRIG_UDP)
 	case transportType == layers.LayerTypeTCP:
 		transportStr = "tcp"
 		tcp := transport.(*layers.TCP)
 		sport = int32(tcp.SrcPort)
 		dport = int32(tcp.DstPort)
+
+		if tcp.FIN == true {
+			triggers = append(triggers, TRIG_TCP_FIN)
+		}
+		if tcp.RST == true {
+			triggers = append(triggers, TRIG_TCP_RST)
+		}
+		if tcp.ACK == true {
+			triggers = append(triggers, TRIG_TCP_ACK)
+		}
+		if tcp.SYN == true {
+			triggers = append(triggers, TRIG_TCP_SYN)
+		}
+		if !tcp.SYN && !tcp.ACK && !tcp.RST && !tcp.FIN {
+			triggers = append(triggers, TRIG_TCP_NSA)
+		}
 	default:
 		transportStr = "unknown"
 	}
+	// XXX remove me
+	//if transportStr != "tcp" {
+	//	return nil, fmt.Errorf("no network type")
+	//}
 	direction = computeDirection(srcIP, dstIP)
+	triggers = append(triggers, direction)
+
 	//log.Printf("%s - %s\n", srcIP.String(), dstIP.String())
 	//log.Printf("%d - %d\n", ip2int(srcIP), ip2int(dstIP))
 	srcIPNum = int32(ip2int(srcIP) >> 24)
@@ -116,16 +197,19 @@ func makeMessage(data []byte, ci gopacket.CaptureInfo) (*osc.Message, error) {
 		appLayerStr = fmt.Sprintf("%s", application.LayerType())
 	}
 
-	message := osc.NewMessage("/gotpacket")
-	message.Append(direction)     // Direction
-	message.Append(int32(length)) // packet length
-	message.Append(sport)         // sport
-	message.Append(dport)         // dport
-	message.Append(appLayerStr)   // highest layer
-	message.Append(transportStr)  // transport layer
-	message.Append(srcIPNum)      // ip_dst
-	message.Append(dstIPNum)      // ip_src
-	return message, nil
+	for _, trig := range triggers {
+		message := osc.NewMessage(getTriggerName(trig))
+		message.Append(int32(direction)) // Direction
+		message.Append(int32(length))    // packet length
+		message.Append(sport)            // sport
+		message.Append(dport)            // dport
+		message.Append(appLayerStr)      // highest layer
+		message.Append(transportStr)     // transport layer
+		message.Append(srcIPNum)         // ip_dst
+		message.Append(dstIPNum)         // ip_src
+		messages = append(messages, message)
+	}
+	return messages, nil
 }
 
 var lastTS time.Time
@@ -139,11 +223,13 @@ func sendPacket(client *osc.Client, data []byte, ci gopacket.CaptureInfo) error 
 	}
 	lastSend = time.Now()
 
-	message, err := makeMessage(data, ci)
+	messages, err := makeMessage(data, ci)
 	if err != nil {
 		log.Printf("Failed to makeMessage: %s\n", err)
 	} else {
-		client.Send(message)
+		for _, msg := range messages {
+			client.Send(msg)
+		}
 	}
 
 	lastTS = ci.Timestamp
